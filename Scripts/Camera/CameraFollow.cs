1ff450512025e4034f7b73ba668baac341519e25
gqu <12128063+Tetimaru@users.noreply.github.com>
2018-12-23 20:07:02 -0800

    Add all files to version control

---
+++ Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform target; // camera will follow the player (player object)
+    public float smoothing = 5f; // camera should lag behind a bit
+    Vector3 offset; // private variable - distance between camera,player
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - target.position;	
+	}
+
+	void FixedUpdate () {
+        // player is also moving in FixedUpdate, camera follows him
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime); // move (smoothly)
+	}
+}
---
+++ Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    // called regardless if script is enabled or not (as opposed to Start)
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // called every Physics update
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal"); // check InputManager
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime; // so user doesn't get advantage moving diagonally (sqrt(1+1) = 1.4)
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) // out = store (function output) information in floorHit var
+        {
+            // if we hit something
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f; // don't want player to lean back (look "up")
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }        
+    }
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
+}
